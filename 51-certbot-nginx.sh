9a34bc37711353facf26bb434037347d2aacc39e
Xarvex <gitlab-github.8qs1z@slmail.me>
2023-11-15 16:54:23 -0600

    Certbot already sets up renewal

--- 51-certbot-nginx.sh
+++ 51-certbot-nginx.sh
@@ -9,9 +9,3 @@ if install_packages certbot python3-certbot-nginx; then
     sudo certbot certonly --nginx -d "$CERTBOT_DOMAIN" -m "$CERTBOT_EMAIL" --agree-tos --non-interactive
     restart_service nginx
 fi
-
-if request_confirmation "Adding certbot renew schedule in cron"; then
-    croncmd="certbot renew --quiet --deploy-hook \"systemctl restart nginx\""
-    cronjob="0 0 * * * $croncmd"
-    ( crontab -l | grep -v -F "$croncmd" || : ; echo "$cronjob" ) | crontab -
-fi
